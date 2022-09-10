ff6824f7e39e701c4367d17fae0d6ac98a21f608
Amustafa2021 <83797423+Amustafa2021@users.noreply.github.com>
2022-09-09 22:47:56 -0500

    Create MustafaAndres_S5C2_RepasoC.cpp

---
+++ MustafaAndres_S5C2_RepasoC.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+
+int main() {
+  int MyNum=69;
+  double MyDec=8.8;
+  cout << "La primera tiene un valor de" << MyNum << "y la segunda tiene un valor de" << MyDec;
+} 
+ 
+  
