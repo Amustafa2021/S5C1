29bd6de9798aac48f5e85eb650a4219d5e9426d9
Amustafa2021 <83797423+Amustafa2021@users.noreply.github.com>
2022-09-10 20:43:08 -0500

    Update MustafaAndres_S5C2_RepasoC.cpp

--- MustafaAndres_S5C2_RepasoC.cpp
+++ MustafaAndres_S5C2_RepasoC.cpp
@@ -1,10 +1,26 @@
+#include <cstdlib>
 #include<iostream>
+#include<cmath>
 using namespace std;
+void exp(float mivarflotante, int mivarentera){
+  return mivarflottante**mivarentera;
+  }
 
 int main() {
   int MyNum=69;
   double MyDec=8.8;
-  cout << "La primera tiene un valor de" << MyNum << "y la segunda tiene un valor de" << MyDec;
+  cout << "La primera tiene un valor de " << MyNum << " y la segunda tiene un valor de" << MyDec;
+  cout << "El resultado es " << MyDec/MyNum;
+  int Arreglo = rand() % 901;
+  
+  for (int i=0;i<300; i+) {
+    cout << Arreglo[i] << "\n";
+  }
+  
+  cout << Arreglo[5];
+  int LongArreglo = sizeof(Arreglo)/sizeof(int);
+  
+  exp(17.5 , 5);
 } 
  
   
