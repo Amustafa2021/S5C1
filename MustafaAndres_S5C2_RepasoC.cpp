bc89270f2f01ebe79367c84b5281a71b879bf0c2
Amustafa2021 <83797423+Amustafa2021@users.noreply.github.com>
2022-09-12 23:25:46 -0500

    Update MustafaAndres_S5C2_RepasoC.cpp

--- MustafaAndres_S5C2_RepasoC.cpp
+++ MustafaAndres_S5C2_RepasoC.cpp
@@ -16,10 +16,14 @@ int main() {
   double MyDec=8.8;
   cout << "La primera tiene un valor de " << MyNum << " y la segunda tiene un valor de" << MyDec;
   cout << "El resultado es " << MyDec/MyNum;
-  Arreglo = rand() % 901;
+  int Arreglo [300];
   
   for (int i=0;i<300; i++) {
-    cout << Arreglo[i] << "\n";
+    Arreglo[i] = rand()%900;
+  }
+
+  for (int i=0;i<300; i++) {
+    cout << Arreglo[i] << " ";
   }
   cout << Arreglo[5];
   int LongArreglo = sizeof(Arreglo)/sizeof(int);
