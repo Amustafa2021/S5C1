f0912aed5bb8c10825287c328c6031cf8645ce0a
Amustafa2021 <Amustafa2021@github.com>
2022-09-13 21:57:32 +0000

    4

--- MustafaAndres_S5C2_RepasoC.cpp
+++ MustafaAndres_S5C2_RepasoC.cpp
@@ -8,26 +8,44 @@ void exp(float mivarflotante, int mivarentera){
   cout << potencia << "\n";
   }
 
+  
 int main() {
   int MyNum=69;
   double MyDec=8.8;
-  cout << "La primera tiene un valor de " << MyNum << " y la segunda tiene un valor de " << MyDec << "\n";
-  cout << "El resultado es " << MyDec/MyNum << "\n";
+  cout << "La primera tiene un valor de: " << MyNum << " y la segunda tiene un valor de: " << MyDec << "\n";
+  cout << "El resultado es: " << MyDec/MyNum << "\n";
   int Arreglo [300];
-  
+    
   for (int i=0;i<300; i++) {
     Arreglo[i] = rand()%900;
   }
 
   for (int i=0;i<300; i++) {
-    cout << Arreglo[i] << " ";
+    cout << Arreglo[i] << ", ";
   }
     
   cout << "\n";
   cout << Arreglo[4] << "\n";
   int LongArreglo = sizeof(Arreglo)/sizeof(int);
-  cout << "La longitud del arreglo es " << LongArreglo << "\n";
+  cout << "La longitud del arreglo es: " << LongArreglo << "\n";
+    
   exp(17.5 , 5);
+    
+  int Menor = Arreglo[0];
+  for (int i=0;i<300; i++) {
+     if (Menor> Arreglo[i]){
+         Menor = Arreglo[i];
+      }
+   }
+  cout << "El menor es: " << Menor << "\n";
+  
+  int i=0;
+  while (Arreglo[i] <801);{
+      if (Arreglo[i]%2 != 0){
+          cout << Arreglo[i] << ", ";
+      }
+      i++;
+  }
 } 
  
   
