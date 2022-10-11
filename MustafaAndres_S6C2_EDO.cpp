4483977fe9e333bd77d8f7c74d421240cbbd70b8
Amustafa2021 <Amustafa2021@github.com>
2022-10-11 20:59:54 +0000

    20

--- MustafaAndres_S6C2_EDO.cpp
+++ MustafaAndres_S6C2_EDO.cpp
@@ -24,9 +24,34 @@ double euler(double t0, double y0, double tf, double h){
     t = t + h;
   }while(t<tf);
 }
- 
+
+double rungekuttafourthdegree(double t0, double y0, double tf, double h){
+  double y = y0;
+  double t = t0;
+  double ka;
+  double kb;
+  double kc;
+  double kd;  
+  double e;
+  do{
+    e = 100*abs(y-analitica(t,y))/analitica(t,y);
+    cout << e << " ";
+    ka =  h*yprima(t,y);
+    kb =  h*yprima(t+h/2,y+ka/2);
+    kc =  h*yprima(t+h/2,y+kb/2);
+    kd =  h*yprima(t+h,y+kc);  
+    y = y + (ka+2*kb+2*kc+kd)/6;
+    t = t + h;
+  }while(t<tf);
+}
+
+
 int main(){
   euler (0,1,1,0.1);
   cout << endl;
   euler (0,1,1,0.01);
+  cout << endl;
+  rungekuttafourthdegree(0,1,1,0.1);
+  cout << endl;
+  rungekuttafourthdegree(0,1,1,0.01);
 }
