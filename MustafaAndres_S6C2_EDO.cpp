99b878bfbb3cbeb3abf5864b53f24349cc8ddc89
Amustafa2021 <83797423+Amustafa2021@users.noreply.github.com>
2022-10-10 20:39:29 -0500

    Update MustafaAndres_S6C2_EDO.cpp

--- MustafaAndres_S6C2_EDO.cpp
+++ MustafaAndres_S6C2_EDO.cpp
@@ -5,7 +5,7 @@
 #include<stdio.h>
 using namespace std;
 
-double analítica(double t, double y){
+double analitica(double t, double y){
   return exp(-t);
 }
 
@@ -16,12 +16,9 @@ double yprima(double t, double y){
 double euler(double t0, double y0, double tf, double h){
   double y = y0;
   double t = t0;
-  double h = h;
-  double tf = tf;
   double e;
   do{
-    cout << y << " ";
-    e = 100*(y-analítica(t,y))/analítica(t,y);
+    e = 100*(y-analitica(t,y))/analitica(t,y);
     cout << e << " ";
     y = y0 + h*yprima(t,y);
     t = t0 + h;
@@ -30,5 +27,6 @@ double euler(double t0, double y0, double tf, double h){
  
 int main(){
   euler (0,1,1,0.1);
-  euler (0,1,1,0.1);
+  cout << endl;
+  euler (0,1,1,0.01);
 }
